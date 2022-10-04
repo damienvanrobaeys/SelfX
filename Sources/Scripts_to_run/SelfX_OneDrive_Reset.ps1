b3bf817dc051656753f6ad504da27a3bb7cfa22f
Damien Van Robaeys <damien.vanrobaeys@gmail.com>
2022-10-04 08:04:20 +0200

    Delete Sources directory

--- Sources/Scripts_to_run/SelfX_OneDrive_Reset.ps1
+++
@@ -1,6 +0,0 @@
-﻿$Get_Current_user = (gwmi win32_computersystem).username
-$Get_Current_user_Name = $Get_Current_user.Split("\")[1]
-$User_Profil_Path = "C:\Users\$Get_Current_user_Name\AppData\Local\Microsoft\OneDrive\OneDrive.exe"	
-Start-Process -FilePath $User_Profil_Path /reset
-sleep 30	
-Start-Process -FilePath $User_Profil_Path /background	
